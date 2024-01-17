7f3ade0c73aacdde2a8bcaf9cbfab22d23ab2f08
Julia Grigoreva <julia_panina@mail.ru>
2024-01-17 17:18:29 +0300

    fix

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,17 +1,21 @@
-const toggleMenu = document.querySelector('.menu-toggle');
-const asideMenu = document.querySelector('.aside-menu');
+const toggleMenu = document.querySelectorAll('.menu-toggle');
+const asideMenu = document.querySelector('.aside-menu-wrapper');
+const bodyEl = document.body;
 
-if (toggleMenu){
-    toggleMenu.addEventListener('click', function(){
+for (let item of toggleMenu) {
+    item.addEventListener('click', function(){
 
         console.log("click!!!")
         
         if(this.classList.contains('active')){
             this.classList.remove('active');
             asideMenu.classList.remove('active');
+            bodyEl.classList.remove('noscroll');
         }else{
             this.classList.add('active');
             asideMenu.classList.add('active');
+            bodyEl.classList.add('noscroll');
         }
     })
 };
+
